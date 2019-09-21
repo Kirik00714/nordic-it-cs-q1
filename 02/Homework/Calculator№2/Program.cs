9eb5a2da3ce1204a571adb6830c5ef5d87e63a50
Андрей <@->
2019-09-21 20:02:57 +0300

    test

---
+++ 02/Homework/Calculator№2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value x");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter operation sign");
+            var sign = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value y");
+            var y = double.Parse(Console.ReadLine());
+            if (sign == '+')
+            {
+                Console.WriteLine($"{x} + {y} = {x + y}");
+            }
+            else if (sign == '-')
+            {
+                Console.WriteLine($"{x} - {y} = {x - y}");
+            }
+            else if (sign == '*')
+            {
+                Console.WriteLine($"{x} * {y} = {x * y}");
+            }
+            else if (sign == '/')
+            {
+                Console.WriteLine($"{x} / {y} = {x / y}");
+            }
+            else if (sign == '%')
+            {
+                Console.WriteLine($"{x} % {y} = {x % y}");
+            }
+            else if (sign == '^')
+            {
+                Console.WriteLine($"{x} ^ {y} = {Math.Pow(x,y)}");
+            }
+            Console.ReadKey();
+        }
+       
+
+    }
+}
