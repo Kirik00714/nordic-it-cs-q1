12e6b4befabdc7068c553c6f43a80ef0748560a6
Андрей <@->
2019-09-21 19:25:30 +0300

    test

---
+++ 02/Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value x");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value y");
+            var y = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{x} + {y} = {x + y} ");
+            Console.WriteLine($"{x} - {y} = {x - y} ");
+            Console.WriteLine($"{x} * {y} = {x * y} ");
+        }
+       
+    }
+}
