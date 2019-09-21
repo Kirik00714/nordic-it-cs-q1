e8f867a1a35a1469acc3de354656eb99d39a2e54
Андрей <@->
2019-09-21 19:01:35 +0300

    test

---
+++ 01/Classwork/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Hello!");
+            Console.ReadKey();
+        }
+    }
+}
