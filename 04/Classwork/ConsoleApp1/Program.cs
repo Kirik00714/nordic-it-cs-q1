e9fd92265d7c58dfd873df96e9de162d561b5834
moscow@inordic.ru <moscow@inordic.ru>
2019-09-26 21:55:34 +0300

    test

---
+++ 04/Classwork/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    /*
+    enum Seasons : int
+    {
+        winter = 3,
+        spring = 6,
+        summer = 9,
+        autumn = 12           
+    };
+    */
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Enter a:");
+            var firstOperand = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter h:");
+            var secondOperand = double.Parse(Console.ReadLine());
+            global::System.Console.WriteLine ($"S(bok) = { 3 * firstOperand * secondOperand}");
+            global::System.Console.WriteLine ($"S(poln) = {1.5 * firstOperand*(firstOperand*Math.Sqrt(3)+ 2*secondOperand)}");
+            global::System.Console.WriteLine($"V ={(Math.Pow(firstOperand,2)/2)* secondOperand * Math.Sqrt(3)}");
+            Console.ReadKey();
+            */
+            /*
+            Seasons today = Seasons.autumn;
+            int SeasonsNumber = (int)today;
+            Console.WriteLine($"{today} is seasons number#{SeasonsNumber}.");
+
+            Seasons today2 = Seasons.spring;
+            int SeasonsNumber2 = (int)today2;
+            Console.WriteLine($"{today2} is seasons number#{SeasonsNumber2}.");
+            Console.ReadKey();
+            */
+
+        }
+         
+
+
+        
+    }
+}
