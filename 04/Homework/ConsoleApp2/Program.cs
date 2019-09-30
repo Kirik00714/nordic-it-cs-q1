aefb7348b139d811f7f4deb99d149c12fd6c6415
Андрей <@->
2019-09-30 18:21:19 +0300

    update

--- 04/Homework/ConsoleApp2/Program.cs
+++ 04/Homework/ConsoleApp2/Program.cs
@@ -18,57 +18,40 @@ namespace ConsoleApp2
             var value = (Cask[])Enum.GetValues(typeof(Cask));
 
             Console.WriteLine("How match juice is required to pach?");
-            var liter = double.Parse(Console.ReadLine());
-            double[] banka = new double[3];
+            var inputValue = double.Parse(Console.ReadLine());
+            double[] caskSize = new double[3];
 
-            banka[2] = Math.Floor(liter / 20);
+            caskSize[2] = Math.Floor(inputValue / 20);
             
-            if (banka[2] != 0)
+            if (caskSize[2] != 0)
             {
-                liter -= banka[2] * 20;
-                if (liter > 5)
-                {
-                    banka[1] = Math.Floor(liter / 5);
-
-                    if (banka[1] != 0)
-                    {
-                        liter -= banka[1] * 5;
-                        if (banka[0] != 0)
-                        {
-                            banka[0] = Math.Ceiling(liter);
-
-                            Console.WriteLine("You will need the following containers");
-                            for (int i = 0; i < value.Length; i++)
-                            {
-                                Console.WriteLine($"{value[i]}: {banka[i]} piece.");
-                            }
-                        }
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("You will need the following containers");
-                        Console.WriteLine($"{value[2]}: {banka[2]} piece.");
-                        Console.WriteLine($"{value[1]}: {banka[1]} piece.");
-                    }
-                    
-                }
-                else
-                {
-                    banka[0] = Math.Ceiling(liter);
-                    Console.WriteLine("You will need the following containers");
-                    Console.WriteLine($"{value[2]}: {banka[2]} piece.");
-                    Console.WriteLine($"{value[0]}: {banka[0]} piece.");
-                }
+                inputValue -= caskSize[2] * 20;
+  
             }
-            else 
+           
+            caskSize[1] = Math.Floor(inputValue / 5);
+
+            if (caskSize[1] != 0)
             {
-                
-                Console.WriteLine("You will need the following containers");
-                Console.WriteLine($"{value[2]}: {banka[2]} piece."); 
+                inputValue -= caskSize[1] * 5;
             }
             
+             caskSize[0] = Math.Ceiling(inputValue);
             
+
+            Console.WriteLine("You will need the following containers");
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if( caskSize[i] > 0)
+                    {
+                    Console.WriteLine($"{value[i]}: {caskSize[i]} piece.");
+                    }
+                   
+                }
+            
+
+
+
         }
     }
 }
