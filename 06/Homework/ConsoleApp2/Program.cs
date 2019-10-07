695bd7e85df9aae4498ee4a8fda5a482793f9567
Андрей <@->
2019-10-07 17:33:15 +0300

    update2.0

--- 06/Homework/ConsoleApp2/Program.cs
+++ 06/Homework/ConsoleApp2/Program.cs
@@ -6,48 +6,52 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the amount of the down payment in rubles:");
-            decimal deposit = default;
-           
             try
             {
-                deposit = decimal.Parse(Console.ReadLine());
-                if (deposit < 0 )
-                {
-                    throw new FormatException("The amount of the down paymen is not correct");
-                }
-            }
-            catch (FormatException exp)
-            {
+                decimal deposit = default;
+                decimal storage = default;
+                decimal income = default;
 
-                Console.WriteLine(exp.Message);
-                Console.WriteLine(exp.StackTrace);
-            }
-            Console.WriteLine("Enter the daily percentage of income as a decimal (1% = 0,01):");
-            decimal income = default;
-            try
-            {
+                Console.WriteLine("Enter the amount of the down payment in rubles:");
+                deposit = decimal.Parse(Console.ReadLine());
+                if (deposit < 0)
+                {
+                    throw new ArgumentException("The amount of the down paymen is not correct");
+                }
+
+                Console.WriteLine("Enter the daily percentage of income as a decimal (1% = 0,01):");
                 income = decimal.Parse(Console.ReadLine());
                 if (income < 0)
                 {
-                    throw new FormatException("The daily percentage of income is not correct");
+                    throw new ArgumentException("The daily percentage of income is not correct");
                 }
-            }
-            catch (FormatException exp)
-            {
 
-                Console.WriteLine(exp.Message);
-                Console.WriteLine(exp.StackTrace);
-            }
-            Console.WriteLine("Enter the desired amount of savings in rubles:");
-            decimal storage = default;
-            try
-            {
+                Console.WriteLine("Enter the desired amount of savings in rubles:");
                 storage = decimal.Parse(Console.ReadLine());
                 if (storage < 0)
                 {
-                    throw new FormatException("The daily percentage of income is not correct");
+                    throw new ArgumentException("The daily percentage of income is not correct");
                 }
+
+                int day = default;
+                while (deposit < storage)
+                {
+                    deposit *= (1 + income);
+                    day++;
+                }
+                Console.WriteLine($"The requires number of days to accumulate the desire amount: {day}");
+            }
+            catch (ArgumentException exp)
+            {
+
+                Console.WriteLine(exp.Message);
+                Console.WriteLine(exp.StackTrace);
+            }
+            catch (OverflowException exp)
+            {
+
+                Console.WriteLine(exp.Message);
+                Console.WriteLine(exp.StackTrace);
             }
             catch (FormatException exp)
             {
@@ -55,13 +59,7 @@ namespace ConsoleApp2
                 Console.WriteLine(exp.Message);
                 Console.WriteLine(exp.StackTrace);
             }
-            int day = default;
-            while (deposit < storage)
-            {
-                deposit *= (1 + income);
-                day++;
-            }
-            Console.WriteLine($"The requires number of days to accumulate the desire amount: {day}");
+            Console.WriteLine("Please enter any key..");
         }
     }
 }
