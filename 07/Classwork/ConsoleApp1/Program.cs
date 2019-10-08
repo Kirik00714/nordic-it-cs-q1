2a4b77e466f7b5f03978a91c66960a5e140dd0e0
ElizavetaChakhlova <elizaveta.chahlova@yandex.ru>
2019-10-08 21:56:49 +0300

    test

---
+++ 07/Classwork/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			Console.WriteLine("enter two real numbers ti multiply them:");
+			var x = double.Parse(Console.ReadLine());
+			var y = double.Parse(Console.ReadLine());
+			Console.WriteLine(x.ToString() + "*" +  y.ToString() + "=" + x*y);
+			Console.WriteLine("{0:##.##} + {1:##.###} = {2:##.###}", x , y , x + y);
+			Console.WriteLine($"{x:#.##} - {y:#.##} = {(x - y):#.##}");
+			*/
+			/*
+			string text = "    lorem   ipsum   dolor      sit   amet  ";
+			var a = text.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+			var b = string.Join(" ", a);
+			var c = new string[a.Length];
+			for (int i = 0; i < a.Length; i++)
+			{
+				c[i] = a[i];
+				if (i == 1)
+				{
+					c[i] = a[i].ToUpper();
+				}
+			}
+			
+			
+			Console.WriteLine(c);
+			Console.ReadKey();
+			*/
+		}
+	}
+}
