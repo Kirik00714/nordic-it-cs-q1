0ca15edf3c031e0f5d0a87d90386d392d8e19850
Андрей <kirik00714@yandex.ru>
2019-10-09 12:31:29 +0300

    update

--- 07/Classwork/ConsoleApp1/Program.cs
+++ 07/Classwork/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			/*
+            /*
 			Console.WriteLine("enter two real numbers ti multiply them:");
 			var x = double.Parse(Console.ReadLine());
 			var y = double.Parse(Console.ReadLine());
@@ -14,24 +14,43 @@ namespace ConsoleApp1
 			Console.WriteLine("{0:##.##} + {1:##.###} = {2:##.###}", x , y , x + y);
 			Console.WriteLine($"{x:#.##} - {y:#.##} = {(x - y):#.##}");
 			*/
-			/*
+            /*
 			string text = "    lorem   ipsum   dolor      sit   amet  ";
-			var a = text.Split(' ',StringSplitOptions.RemoveEmptyEntries);
-			var b = string.Join(" ", a);
-			var c = new string[a.Length];
-			for (int i = 0; i < a.Length; i++)
+			var words = text.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+			var result = new string[words.Length];
+			for (int i = 0; i < result.Length; i++)
 			{
-				c[i] = a[i];
+				result[i] = words[i];
 				if (i == 1)
 				{
-					c[i] = a[i].ToUpper();
+					result[i] = words[i].ToUpper();
 				}
 			}
+            Console.WriteLine(string.Join(" ", result));
+
+            var words1 = text.TrimEnd();
+            int index = words1.LastIndexOf(' ');
+            words1 = words1.Substring(0, index);
+            words1 = words1.TrimEnd();
+            Console.WriteLine(words1);
+            */
+
+            
+            var text = new StringBuilder ("    lorem   ipsum   dolor      sit   amet  ");
+            text.Remove(0, 4);
+            text.Remove(5, 2);
+            text.Remove(11, 2);
+            text.Remove(17, 5);
+            text.Remove(21, 2);
+            text.Remove(25, 0);
+            text.Replace("ipsum", "IPSUM");
+            Console.WriteLine(text.ToString());
+
+            var str = text.Remove(21, 7);
+            text.Replace("IPSUM", "ipsum");
+            Console.WriteLine(str.ToString());
+            Console.ReadKey();
 			
-			
-			Console.WriteLine(c);
-			Console.ReadKey();
-			*/
 		}
 	}
 }
