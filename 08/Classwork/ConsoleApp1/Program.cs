38a85a305725b523b250c06acfb30ce8f75b68b7
ElizavetaChakhlova <elizaveta.chahlova@yandex.ru>
2019-10-10 22:04:22 +0300

    test

---
+++ 08/Classwork/ConsoleApp1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			Console.WriteLine("Enter numbers");
+			var numbers = new List<double>();
+			double result = 0;
+			
+			while (true)
+			{
+				try
+				{
+					var value = Console.ReadLine();
+					
+					if (value.ToLower() == "stop")
+					{
+						break;
+					}
+					else
+					{
+						numbers.Add(double.Parse(value));
+					}
+				}
+				catch (FormatException vl)
+				{
+
+					Console.WriteLine(vl.Message);
+				}
+				
+				
+			}
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				result += numbers[i] / numbers.Count;
+			}
+			Console.WriteLine(result);
+			Console.ReadKey();
+			*/
+			/*
+			var capitals = new Dictionary<string, string>
+			{
+				["Russia"] = "Moscow",
+				["Great Britain"] = "London",
+				["USA"] = "Washington",
+				["China"] = "Pekin",
+
+			};
+
+			
+			KeyValuePair <string, string > randomElement =GetRandomElement(capitals);
+			while (true)
+			{
+				Console.WriteLine($"Enter capital {randomElement.Key}");
+				var capital = Console.ReadLine();
+				if (randomElement.Key == capital)
+				{
+					Console.WriteLine("Nice");
+				}
+				
+				break;
+				
+			}
+			
+			Console.ReadKey();
+			*/
+
+			Console.WriteLine("Enter number:");
+			Queue<double> numbers = new Queue<double>();
+			while (true)
+			{
+				var value = Console.ReadLine();
+				if (value == "run")
+				{
+					
+					while (numbers.Count > 0)
+					{
+						double number = numbers.Dequeue();
+						number = Math.Sqrt(number);
+						Console.WriteLine(number);
+					}
+				}
+				else if (value == "exit")
+				{
+					break;
+				}
+				else if (value != "run" || value != "exit") 
+				{
+					numbers.Enqueue(int.Parse(value));
+				}
+			}
+
+
+
+
+
+		}
+		/*
+		private static KeyValuePair<string, string> GetRandomElement(Dictionary<string, string> capitals)
+		{
+			var random = new Random();
+			var i = 0;
+			var index = random.Next(0, capitals.Count);
+			foreach (var pair in capitals)
+			{
+				if (i == index)
+				{
+					return pair;
+				}
+				i++;
+			}
+			throw new IndexOutOfRangeException();
+		}
+		*/
+
+
+
+	}
+}
