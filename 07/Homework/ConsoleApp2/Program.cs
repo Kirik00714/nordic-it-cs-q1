dc71c599c7c07754af57c990dabf2bde88a632f3
Андрей <kirik00714@yandex.ru>
2019-10-10 17:13:04 +0300

    test

---
+++ 07/Homework/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter string:");
+            var word = Console.ReadLine();
+            try
+            {
+                foreach (var err in word)
+                {
+                    if (err == '1' || err == '2' || err == '3' || err == '4' || err == '5' || err == '6' || err == '7' || err == '8' || err == '9' || err == '0')
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+                if (word == " ")
+                {
+                    Console.WriteLine("You  entered an empty string");
+                }
+                for (int i = word.Length -1; i >= 0; i--)
+                {
+                    Console.WriteLine(word[i]);
+                }
+                Console.WriteLine("\nPress ane key to exit");
+                Console.ReadKey();
+            }
+            catch (ArgumentException wd)
+            {
+
+                Console.WriteLine(wd.Message);
+
+            }
+        }
+    }
+}
