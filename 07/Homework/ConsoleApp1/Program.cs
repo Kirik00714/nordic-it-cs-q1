edd6c6c22eaf6ecbd47169179367af08c28fe942
Андрей <kirik00714@yandex.ru>
2019-10-12 13:36:46 +0300

    update2.0

--- 07/Homework/ConsoleApp1/Program.cs
+++ 07/Homework/ConsoleApp1/Program.cs
@@ -6,35 +6,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter string:");
-            var word = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            try
+            while (true)
             {
-                foreach (var err in word)
+                Console.WriteLine("Please enter string:");
+                var word = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                try
                 {
-                    if (err[0] == '1' || err[0] == '2' || err[0] == '3' || err[0] == '4' || err[0] == '5' || err[0] == '6' || err[0] == '7' || err[0] == '8' || err[0] == '9' || err[0] == '0')
+                    foreach (var err in word)
                     {
-                        throw new ArgumentException();
+                        if (err[0] == '1' || err[0] == '2' || err[0] == '3' || err[0] == '4' || err[0] == '5' || err[0] == '6' || err[0] == '7' || err[0] == '8' || err[0] == '9' || err[0] == '0')
+                        {
+                            throw new ArgumentException();
+                        }
                     }
-                }
-                if (word.Length <= 1)
-                {
-                    Console.WriteLine("You have entered too few words");
-                }
-                int count = default;
-                foreach (var words in word)
-                {
-                    if (words[0] == 'A' || words[0] == 'a' || words[0] == 'А' || words[0] == 'а')
+                    if (word.Length <= 1)
                     {
-                        count++; ;
+                        Console.WriteLine("You have entered too few words");
                     }
+                    int count = default;
+                    foreach (var words in word)
+                    {
+                        if (words[0] == 'A' || words[0] == 'a' || words[0] == 'А' || words[0] == 'а')
+                        {
+                            count++; ;
+                        }
+                    }
+                    Console.WriteLine($"Numbers of words beginning with a lette <А>:{count}");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey();
+                    break;
                 }
-                Console.WriteLine("Press any key to exit");
-                Console.ReadKey();
-            }
-            catch (ArgumentException wd)
-            {
-                Console.WriteLine(wd.Message);
+
+                catch (ArgumentException wd)
+                {
+                    Console.WriteLine(wd.Message);
+                }
             }
         }
     }
