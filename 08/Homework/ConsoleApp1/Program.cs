9afad5413256098e2046258f0ef3a18a34874831
Андрей <kirik00714@yandex.ru>
2019-10-14 20:26:47 +0300

    update

--- 08/Homework/ConsoleApp1/Program.cs
+++ 08/Homework/ConsoleApp1/Program.cs
@@ -7,31 +7,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
             Queue<string> bracket = new Queue<string>();
             int count = default;
-            int count1 = default;
-            int count2 = default;
+            var value = Console.ReadLine();
             while (true)
             {
-                var value = Console.ReadLine();
-                bracket.Enqueue((value));
-                count = Outputbracket(count, value);
-                count1 = Outputbracket1(count1, value);
-                count2 = Outputbracket2(count2, value);
-                if (count == 0 && count1 == 0 && count2 == 0 )
-                {
-                    Console.WriteLine(true);
-                }
-                else
-                {
-                    Console.WriteLine(false);
-                }
 
+                bracket.Enqueue((value));
+                if (value == "go")
+                {
+                    count = Outputbracket(count, value);
+
+                    if (count == 0)
+                    {
+                        Console.WriteLine(true);
+                    }
+                    else
+                    {
+                        Console.WriteLine(false);
+                    }
+
+                }
             }
         }
 
-         static int Outputbracket(int count, string value)
+        static int Outputbracket(int count, string value)
         {
 
             while (true)
@@ -48,39 +48,16 @@ namespace ConsoleApp1
                 return count;
             }
         }
-        static int Outputbracket1(int count1, string value)
-        {
+       
+        
 
-            while (true)
-            {
-                if (value == "{")
-                {
-                    count1++;
-                }
-                else if (value == "}")
-                {
-                    count1--;
-                }
 
-                return count1;
-            }
-        }
-        static int Outputbracket2(int count2, string value)
-        {
 
-            while (true)
-            {
-                if (value == "[")
-                {
-                    count2++;
-                }
-                else if (value == "]")
-                {
-                    count2--;
-                }
+        
 
-                return count2;
-            }
-        }
+         
+        
+       
+        
     }
 }
