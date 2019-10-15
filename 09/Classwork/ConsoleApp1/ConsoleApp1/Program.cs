4604a151fa06d7802095ce651362ffe4635c88c6
yvnovikova@mail.ru <a@email.ru>
2019-10-15 22:04:15 +0300

    test

---
+++ 09/Classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stopwatch time = new Stopwatch();
+			var arr = new int[50000];
+			var rnd = new Random();
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = rnd.Next();
+			}
+			
+				time.Start();
+			BubleSort(arr);
+			time.Stop();
+				Console.WriteLine($" Time1: {time.ElapsedMilliseconds}");
+				time.Reset();
+				time.Start();
+				Array.Sort(arr, (x, y) => x.CompareTo(y));
+				time.Stop();
+				Console.WriteLine($" Time2: {time.ElapsedMilliseconds}");
+				Console.ReadKey();
+			}
+			
+			static void BubleSort(int[] arr)
+			{
+				for (int i = 0; i < arr.Length - 1; i++)
+				{
+					var limit = arr.Length - 1 - i;
+					for (int j = 0; j < limit; j++)
+					{
+						if (arr[j]> arr[j+1])
+						{
+							var temp = arr[j +1];
+							arr[j + 1] = arr[j];
+							arr[j]  = temp;
+						}
+					}
+
+				}
+
+
+			}
+			
+		}
+}
