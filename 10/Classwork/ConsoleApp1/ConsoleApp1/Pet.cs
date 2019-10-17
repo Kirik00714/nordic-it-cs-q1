dd73612b7c34ba2cbed78e0bea1518296a2f6d13
Slayer143 <niki2025@mail.ru>
2019-10-17 22:10:07 +0300

    test

---
+++ 10/Classwork/ConsoleApp1/ConsoleApp1/Pet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Pet
+	{
+		public string Kind;
+		public string Name;
+		public char Sex;
+		public int Age;
+
+		/*
+		public string GetDescription 
+		{
+			get
+			{
+				return $"The pet kind of {Kind}, has name {Name}, age {Age}, sex {Sex}";
+			}
+		}
+		*/
+		public string GetDescription =>
+			 $"The pet kind of {Kind}, has name {Name}, age {Age}, sex {Sex}";
+		
+	}
+}
---
+++ 10/Classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Pet pet1 = new Pet();
+			pet1.Name = "The Cat";
+			pet1.Kind = "Cat";
+			pet1.Sex = 'F';
+			pet1.Age = 10;
+
+			Pet pet2 = new Pet()
+			{
+				Name = "The Dog",
+				Kind = "Dogt",
+				Sex = 'M',
+				Age = 10,
+			};
+
+			Console.WriteLine(pet1.GetDescription);
+			Console.WriteLine(pet2.GetDescription);
+
+			
+			
+			Console.WriteLine($"The pet kind of {pet1.Kind}, has name {pet1.Name}, age {pet1.Age}, sex {pet1.Sex}");
+			Console.WriteLine($"The pet kind of {pet2.Kind}, has name {pet2.Name}, age {pet2.Age}, sex {pet2.Sex}");
+		}
+	}
+}
