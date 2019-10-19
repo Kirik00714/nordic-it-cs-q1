a492a8b0597b70284273c0f9b0cc44fd752c2371
Андрей <kirik00714@yandex.ru>
2019-10-19 18:53:19 +0300

    update 3.0

--- 08/Homework/ConsoleApp1/Program.cs
+++ 08/Homework/ConsoleApp1/Program.cs
@@ -6,74 +6,49 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Enter a string containing parenthese. For exemple '(', ')', '[', ']'");
-            Stack<char> bracket = new Stack<char>();
-            Stack<char> bracket1 = new Stack<char>();
-
+        {   
+            Console.WriteLine("Enter a string containing parenthese. For exemple '(', ')', '[', ']', '{', '}'");
             var str = Console.ReadLine();
+            Console.WriteLine(CountBracket(str));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
 
-            foreach (var ch in str)
+         static bool CountBracket(string line)
+         {
+            Stack<char> bracket = new Stack<char>();
+           
+            foreach (var ch in line)
             {
                 if (ch == '(')
                 {
                     bracket.Push(ch);
+                    
                 }
                 else if (ch == ')')
                 {
-                    if (bracket.Count == 0)
+                    if (bracket.Count == 0 || bracket.Peek() != ')' )
                     {
-                        Console.WriteLine(false);
-                        break;
+                        return false;
+                        
                     }
-
                     else
                     {
                         bracket.Pop();
+                        
                     }
                 }
-
-
-                if (ch == '[')
-                {
-
-                    bracket1.Push(ch);
-
-                }
-                else if (ch == ']')
-                {
-                    if (bracket1.Count == 0)
-                    {
-                        Console.WriteLine(false);
-                        break;
-                    }
-
-                    else
-                    {
-                        bracket1.Pop();
-                    }
-                }
-
             }
-            if (bracket.Count == 0 && bracket1.Count == 0)
+            if (bracket.Count == 0)
             {
-                Console.WriteLine(true);
+                return true;
             }
-            else
-            {
-                Console.WriteLine(false);
-            }
-
-
-
-
-
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
-        }
+            return bracket.Count == 0;
+            
+         }
 
     }
 
 
-}
+
 }
