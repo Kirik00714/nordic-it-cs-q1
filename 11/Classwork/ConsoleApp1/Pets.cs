6514b6e3f3323899e84978982834ccef0c4deca0
Mikhail <migel_sordona@mail.ru>
2019-10-22 21:49:12 +0300

    test

---
+++ 11/Classwork/ConsoleApp1/Pets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	public class Pets
+	{
+		public string Kind;
+		public string Name;
+		public char Sex;
+		public DateTime DateOfBirth;
+		public Pets(string name, string kind)
+		{
+			Name = name;
+			Kind = kind;
+		}
+		
+		public byte GetAge()
+		{
+			var date = DateTime.Now;
+			TimeSpan different = date - DateOfBirth;
+			return (byte) Math.Floor(different.TotalDays / 365);
+		}
+		public string Description
+		{
+			get { return $"The pet kind of { Kind}, has name { Name}, age { GetAge()}, sex { Sex} "; }
+		}
+		public string ShortDescription
+		{
+			get { return $"The pet kind of { Kind}, has name { Name}"; }
+		}
+		public void Display(string str)
+		{
+			
+			if (str == "Full")
+			{
+				Console.WriteLine(Description);
+			}
+			else if (str == "Short")
+			{
+				Console.WriteLine(ShortDescription);
+			}
+		}
+			
+	}
+}
---
+++ 11/Classwork/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var pet1 = new Pets("Bobic" , " Dog");
+			
+			pet1.Sex = 'M';
+			pet1.DateOfBirth = new DateTime(2015, 3, 23);
+			Console.WriteLine(pet1.Description);
+
+			var pet2 = new Pets("Vasya","Cat")
+			{
+				
+				Sex = 'F',
+				DateOfBirth = new DateTime(2018, 9, 15)
+			};
+			Console.WriteLine("Enter <Full> for long description or <Short> for short decription");
+			string str = Console.ReadLine(); ;
+			pet1.Display(str);
+				Console.WriteLine(pet2.Description);
+			var pets = new Pets[2];
+			for (int i = 0; i < pets.Length; i++)
+			{
+				pets[i] = new Pets(name: Console.ReadLine(),
+					kind: Console.ReadLine() );
+			}
+			for (int i = 0; i < pets.Length; i++)
+			{
+				Console.WriteLine(pets[i].ShortDescription);
+			}
+				Console.ReadKey();
+			
+			}
+	}
+	
+}
