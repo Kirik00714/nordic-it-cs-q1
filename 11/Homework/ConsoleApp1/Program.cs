2ff377d3f0dc00763493a8b4f66856a13e3426d7
Андрей <kirik00714@yandex.ru>
2019-10-26 17:05:04 +0300

    test

--- 11/Homework/ConsoleApp1/Program.cs
+++ 11/Homework/ConsoleApp1/Program.cs
@@ -2,40 +2,6 @@
 
 namespace ConsoleApp1
 {
-    class ReminderItem
-    {
-        public ReminderItem(DateTime alarmDate, string alarmMessage)
-        {
-            AlarmDate = alarmDate;
-            AlarmMessage = alarmMessage;
-        }
-
-        public DateTime AlarmDate { get; set; }
-        public string AlarmMessage { get; set; }
-        public TimeSpan TimeToAlarm =>
-            AlarmDate - DateTime.Now;
-        public bool IsOutdated
-        {
-            get
-            {
-                if (TimeToAlarm >= TimeSpan.Zero)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-        public void WriteProperties()
-        {
-            Console.WriteLine($"AlarmDate: {AlarmDate}");
-            Console.WriteLine($"AlarmMessage: {AlarmMessage}");
-            Console.WriteLine($"TimeToAlarm: {TimeToAlarm}");
-            Console.WriteLine($"IsOutdated: {IsOutdated}");    
-        }
-    }
     class Program
     {
         static void Main(string[] args)
---
+++ 11/Homework/ConsoleApp1/ReminderItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ReminderItem
+    {
+        public ReminderItem(DateTime alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public DateTime AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm =>
+            AlarmDate - DateTime.Now;
+        public bool IsOutdated
+        {
+            get
+            {
+                if (TimeToAlarm >= TimeSpan.Zero)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public void WriteProperties()
+        {
+            Console.WriteLine($"AlarmDate: {AlarmDate}");
+            Console.WriteLine($"AlarmMessage: {AlarmMessage}");
+            Console.WriteLine($"TimeToAlarm: {TimeToAlarm}");
+            Console.WriteLine($"IsOutdated: {IsOutdated}");    
+        }
+    }
+}
