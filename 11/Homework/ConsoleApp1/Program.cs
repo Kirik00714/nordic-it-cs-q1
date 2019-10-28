e7383330b4732667bab989d39aff8f0b8df0f1e3
Андрей <kirik00714@yandex.ru>
2019-10-28 19:26:20 +0300

    update

--- 11/Homework/ConsoleApp1/Program.cs
+++ 11/Homework/ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
             {
             };
             reminderItem.WriteProperties();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
