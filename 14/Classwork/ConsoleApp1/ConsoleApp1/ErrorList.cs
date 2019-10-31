3d6eba06c925ab130191c5fc324b5cccf7763d6d
Slayer143 <niki2025@mail.ru>
2019-10-31 22:02:37 +0300

    test

---
+++ 14/Classwork/ConsoleApp1/ConsoleApp1/ErrorList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+	class ErrorList : IDisposable, IEnumerable<string>
+	{
+		public ErrorList(string category, List<string> errors)
+		{
+			Category = category;
+			Errors = errors;
+		}
+
+		public string Category { get; set; }
+		private List<string> Errors; 
+		public void Dispose()
+		{
+			Errors.Clear();
+			Errors = null;
+		}
+		public void Add(string errorMEssage)
+		{
+			Errors.Add(errorMEssage);
+			
+		}
+
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			return Errors.GetEnumerator() ;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
---
+++ 14/Classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var errorlist = new ErrorList("Number", new List<string>()))
+			{
+				errorlist.Add("1");
+				errorlist.Add("2");
+				foreach (var item in errorlist )
+				{
+					Console.WriteLine($"Category:{errorlist.Category},Error:{item}");
+				}
+				Console.ReadKey();
+			}
+
+		}
+	}
+}
