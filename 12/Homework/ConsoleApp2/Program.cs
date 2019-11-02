e8f9d722130ae7318843e334b932779df009b415
Андрей <kirik00714@yandex.ru>
2019-11-02 18:05:21 +0300

    update 2.0

--- 12/Homework/ConsoleApp2/Program.cs
+++ 12/Homework/ConsoleApp2/Program.cs
@@ -6,19 +6,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            var Documents = new BaseDocument[3];
-            Documents[0] = new BaseDocument("Pass", "489-589-65-68",new DateTime(2019, 10, 29, 9, 00, 00));
-            Documents[1] = new Passport("USA", "Kirill",new DateTime(2012, 03, 15, 10, 30, 00), "489-563");
-            Documents[2] = new Passport("Norway", "Mark", new DateTime(2015, 06, 28, 14, 26, 00), "064-088");
+            var documents = new BaseDocument[3];
+            documents[0] = new BaseDocument("Pass", "489-589-65-68",new DateTime(2019, 10, 29, 9, 00, 00));
+            documents[1] = new Passport("USA", "Kirill",new DateTime(2012, 03, 15, 10, 30, 00), "489-563");
+            documents[2] = new Passport("Norway", "Mark", new DateTime(2015, 06, 28, 14, 26, 00), "064-088");
 
-            for (int i = 0; i < Documents.Length; i++)
+            for (int i = 0; i < documents.Length; i++)
             {
-                if (Documents[i] is Passport passport)
+                if (documents[i] is Passport passport)
                 {
                     passport.ChangeIssueDate(DateTime.Now);
-                    Documents[i].IssueDate = passport.IssueDate;
                 }
-                Documents[i].WriteToConsole();
+                documents[i].WriteToConsole();
                 Console.WriteLine("");
             }
             Console.WriteLine("Press any key to exit...");
