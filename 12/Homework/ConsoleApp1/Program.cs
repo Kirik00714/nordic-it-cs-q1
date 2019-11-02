bdbb8c318701bd9e0679b9e027e94c21cf55c19f
Андрей <kirik00714@yandex.ru>
2019-11-02 18:00:22 +0300

    update

--- 12/Homework/ConsoleApp1/Program.cs
+++ 12/Homework/ConsoleApp1/Program.cs
@@ -7,16 +7,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var Reminders = new List<ReminderItem>
+            var reminders = new List<ReminderItem>
             {
-                new ReminderItem(new DateTime(2019, 10, 30, 9, 30, 00), "Good morning! Time to get up!"),
+                new ReminderItem(new DateTime(2099, 10, 30, 9, 30, 00), "Good morning! Time to get up!"),
                 new PhoneReminderItem(new DateTime(2020, 01, 01, 00, 00, 01), "Happy New Year", "+7(916) 777-77-77"),
                 new ChatReminderItem(new DateTime(2019, 11, 12, 16, 00, 00), "Ask homework", "Friends", "What homework?")
             };
 
-            for (int i = 0; i < Reminders.Count; i++)
+            for (int i = 0; i < reminders.Count; i++)
             {
-                Reminders[i].WriteProperties();
+                reminders[i].WriteProperties();
                 Console.WriteLine("");
             }
 
