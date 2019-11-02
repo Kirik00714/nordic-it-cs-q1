34b005928525bbdb917150658c84329a619bdd8f
Андрей <kirik00714@yandex.ru>
2019-11-02 17:56:15 +0300

    update 2.0

--- 11/Homework/ConsoleApp1/Program.cs
+++ 11/Homework/ConsoleApp1/Program.cs
@@ -6,10 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            ReminderItem reminderItem = new ReminderItem(new DateTime(2019, 10, 29, 16, 52, 30), "Good morning! Time to get up!")
+            var reminderItem = new ReminderItem(new DateTime(2019, 10, 29, 16, 52, 30), "Good morning! Time to get up!")
             {
             };
             reminderItem.WriteProperties();
+            var reminderItem1 = new ReminderItem(new DateTime(2099, 12, 02, 16, 52, 30), "Good morning! Time to get up!")
+            {
+            };
+            reminderItem1.WriteProperties();
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
