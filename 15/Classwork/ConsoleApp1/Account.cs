f8267e9e761a07b4f73ea919f3d1b2af06278359
ElizavetaChakhlova <elizaveta.chahlova@yandex.ru>
2019-11-05 21:55:53 +0300

    test

---
+++ 15/Classwork/ConsoleApp1/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Account<T>
+	{
+		public Account(T id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+
+		public T Id { get; private set; }
+		public string Name { get; private set; }
+		public void WriteProperties() => 
+			Console.WriteLine($"Id:{Id}, Name:{Name}");
+
+	}
+}
---
+++ 15/Classwork/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var Acc = new Account<int>(2,"Andrey");
+			Acc.WriteProperties();
+			var Acc1 = new Account<string>("32","Sasha");
+			Acc1.WriteProperties();
+			Guid.NewGuid();
+			var Acc2 = new Account<Guid>(Guid.NewGuid(),"Nikita");
+			Acc2.WriteProperties();
+			Console.ReadKey();
+
+		}
+	}
+}
