f3716611c6e4d361096a09f364fe14070e314186
Андрей <kirik00714@yandex.ru>
2019-11-11 22:57:30 +0300

    update

---
+++ 15/Homework/ConsoleApp2/LogWriters/LogWriterFactory.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp2
+{
+    class LogWriterFactory
+    {
+        private static LogWriterFactory instance;
+        private LogWriterFactory()
+        { }
+        public static LogWriterFactory GetInstance()
+        {
+            return instance ??
+                (instance = new LogWriterFactory());
+        }
+        public ILogWriter GetLogWriter<T>(object parameters) where T : ILogWriter
+        {
+
+            if (typeof(T) == typeof(ConsoleLogWriter))
+            {
+                return new ConsoleLogWriter();
+            }
+            else if (typeof(T) == typeof(FileLogWriter))
+            {
+                if (!string.IsNullOrWhiteSpace((string)parameters))
+                {
+                    return new FileLogWriter((string)parameters);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else if (typeof(T) == typeof(MiltipleLogWriter))
+            {
+                return new MiltipleLogWriter((ILogWriter[])parameters);
+            }
+            else
+            {
+                return null;
+            }
+
+        }       
+
+    }
+    
+}
--- 15/Homework/ConsoleApp2/Program.cs
+++ 15/Homework/ConsoleApp2/Program.cs
@@ -2,73 +2,32 @@
 
 namespace ConsoleApp2
 {
-    class LogWriterFactory
+    class Program
     {
-        private static LogWriterFactory instance;
-        private LogWriterFactory()
-        { }
-        public static LogWriterFactory GetInstance()
+        static void Main(string[] args)
         {
-            return instance ??
-                (instance = new LogWriterFactory());
-        }
-        public ILogWriter GetLogWriter<T>(object parameters) where T : ILogWriter
-        {
-            
-                if (typeof(T) == typeof(ConsoleLogWriter))
-                {
-                    return new ConsoleLogWriter();
-                }
-                else if (typeof(T) == typeof(FileLogWriter))
-                {
-                    if (!string.IsNullOrWhiteSpace((string)parameters))
-                    {
-                        return new FileLogWriter((string)parameters);
-                    }
-                    else
-                    {
-                    return null;
-                    }
-                }
-                else if (typeof(T) == typeof(MiltipleLogWriter))
-                {
-                return new MiltipleLogWriter((ILogWriter[])parameters);
-                }
-                else
-                {
-                return null;
-                }
-            
-            
+            var logwriterfactory = LogWriterFactory.GetInstance();
+            var consolelogwriter = logwriterfactory.GetLogWriter<ConsoleLogWriter>(null);
+            consolelogwriter.LogInfo("Message for Info");
+            consolelogwriter.LogWarning("Message for Warning");
+            consolelogwriter.LogError("Message for Error");
 
-        }
+            var filelogwriter = logwriterfactory.GetLogWriter<FileLogWriter>("File.txt");
+            filelogwriter.LogInfo("Message for Info");
+            filelogwriter.LogWarning("Message for Warning");
+            filelogwriter.LogError("Message for Error");
+            Console.WriteLine("");
 
-        class Program
-        {
-            static void Main(string[] args)
-            {
-                var logwriterfactory = LogWriterFactory.GetInstance();
-                var consolelogwriter = logwriterfactory.GetLogWriter<ConsoleLogWriter>(null);
-                consolelogwriter.LogInfo("Message for Info");
-                consolelogwriter.LogWarning("Message for Warning");
-                consolelogwriter.LogError("Message for Error");
+            ILogWriter[] ilogwriters = new ILogWriter[] { consolelogwriter, filelogwriter };
 
-                var filelogwriter = logwriterfactory.GetLogWriter<FileLogWriter>("File.txt");
-                filelogwriter.LogInfo("Message for Info");
-                filelogwriter.LogWarning("Message for Warning");
-                filelogwriter.LogError("Message for Error");
-                Console.WriteLine("");
+            var miltilogwriter = logwriterfactory.GetLogWriter<MiltipleLogWriter>(ilogwriters);
+            miltilogwriter.LogInfo("Message for Info");
+            miltilogwriter.LogWarning("Message for Warning");
+            miltilogwriter.LogError("Message for Error");
 
-                ILogWriter[] ilogwriters = new ILogWriter[] { consolelogwriter, filelogwriter };
-
-                var miltilogwriter = logwriterfactory.GetLogWriter<MiltipleLogWriter>(ilogwriters);
-                miltilogwriter.LogInfo("Message for Info");
-                miltilogwriter.LogWarning("Message for Warning");
-                miltilogwriter.LogError("Message for Error");
-
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
+    
 }
